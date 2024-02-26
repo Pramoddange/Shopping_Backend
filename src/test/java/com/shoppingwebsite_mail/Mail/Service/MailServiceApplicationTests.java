550b2d01f3b5e703dc7396a2d51d94bb66183caa
Pramod Dange <pramodsdange1@gmail.com>
2024-02-26 22:13:47 +0530

    initial code mail service added

--- src/test/java/com/shoppingwebsite_mail/Mail/Service/MailServiceApplicationTests.java
+++ src/test/java/com/shoppingwebsite_mail/Mail/Service/MailServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.shopping.database.api.Database.Ap;
+package com.shoppingwebsite_mail.Mail.Service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DatabaseApApplicationTests {
+class MailServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
